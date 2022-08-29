d4f02bc1bf370cf70f56bdc91b36b06650f26afe
Dani Edo <dani.nur.afandi@gmail.com>
2022-08-29 23:57:21 +0700

    render order

--- screens/Restaurant.tsx
+++ screens/Restaurant.tsx
@@ -11,6 +11,7 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import { isIphoneX } from "react-native-iphone-x-helper";
 import { COLORS, FONTS, icons, SIZES } from "../constants";
 import { CurrentLocationType, RestaurantDataType } from "../types/general";
 
@@ -265,7 +266,103 @@ const Restaurant: React.FunctionComponent<Props> = ({ navigation, route }) => {
   };
 
   const renderOrder = () => {
-    return <View>{renderDots()}</View>;
+    return (
+      <View>
+        {renderDots()}
+        <View
+          style={{
+            backgroundColor: COLORS.white,
+            borderTopLeftRadius: 40,
+            borderTopRightRadius: 40,
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              paddingVertical: SIZES.padding * 2,
+              paddingHorizontal: SIZES.padding * 3,
+              borderBottomColor: COLORS.lightGray2,
+              borderBottomWidth: 1,
+            }}
+          >
+            <Text style={{ ...FONTS.h3 }}>items in Cart</Text>
+            <Text style={{ ...FONTS.h3 }}>$45</Text>
+          </View>
+          <View
+            style={{
+              paddingVertical: SIZES.padding * 2,
+              paddingHorizontal: SIZES.padding * 3,
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <View style={{ flexDirection: "row" }}>
+              <Image
+                source={icons.pin}
+                resizeMode="contain"
+                style={{ width: 20, height: 20, tintColor: COLORS.darkgray }}
+              />
+              <Text
+                style={{
+                  marginLeft: SIZES.padding,
+                  ...FONTS.h3,
+                }}
+              >
+                Location
+              </Text>
+            </View>
+            <View style={{ flexDirection: "row" }}>
+              <Image
+                source={icons.master_card}
+                resizeMode="contain"
+                style={{ width: 20, height: 20, tintColor: COLORS.darkgray }}
+              />
+              <Text
+                style={{
+                  marginLeft: SIZES.padding,
+                  ...FONTS.h4,
+                }}
+              >
+                8888
+              </Text>
+            </View>
+
+            {/* Order Button */}
+          </View>
+          <View
+            style={{
+              padding: SIZES.padding * 2,
+              alignItems: "center",
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                width: SIZES.width * 0.9,
+                backgroundColor: COLORS.primary,
+                alignItems: "center",
+                padding: SIZES.padding,
+                borderRadius: SIZES.radius,
+              }}
+            >
+              <Text style={{ color: COLORS.white, ...FONTS.h2 }}>Order</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        {isIphoneX() && (
+          <View
+            style={{
+              position: "absolute",
+              left: 0,
+              right: 0,
+              bottom: -34,
+              height: 34,
+              backgroundColor: COLORS.white,
+            }}
+          ></View>
+        )}
+      </View>
+    );
   };
 
   return (
